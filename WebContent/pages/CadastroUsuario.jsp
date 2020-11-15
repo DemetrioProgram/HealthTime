89d2baa2158dcf5a970f5c283c5e8a69086b3654
gsmartins <smmachado.gabriel@gmail.com>
2020-11-15 19:29:49 -0300

    Tela cadastro usuario

---
+++ WebContent/pages/CadastroUsuario.jsp
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="ISO-8859-1" ?>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<title>Cadastrar Usuario</title>
+</head>
+
+
+<body>
+
+
+<form action="CadastroUsuario" method="post">
+
+<table id="tblPrincipal" border="1">
+
+<tr>
+<td>
+
+<h3 id="titulo" align="center">Efetue seu Cadastro</h3>
+<table id="tblCadastro" align="center">
+
+<tr>
+<td align="right"><label class="fonte" >Nome:</label> </td>
+<td align="left"><input type="text" size="70" name="txtNome" required/></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">CPF:</label> </td>
+<td align="left"><input type="text" size="20" name="txtNuCf" required/></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Data Nascimento:</label> </td>
+<td align="left"><input type="text" name="txtDtNascimento" required/></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">E-mail:</label> </td>
+<td align="left"><input type="text" name="txtEmail" required/></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Gestante:</label> </td>
+<td align="left"><input type="checkbox" name="flGestante" /></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Idoso:</label> </td>
+<td align="left"><input type="checkbox" name="flIdoso" /></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Menor de Idade:</label> </td>
+<td align="left"><input type="checkbox" name="flMenorIdade" /></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Pessoa com Nessecidades Especiais:</label> </td>
+<td align="left"><input type="checkbox" name="flPne" /></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Nome da Mãe:</label> </td>
+<td align="left"><input type="text" name="txtNmMae" reuired/></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Tel. Contato:</label> </td>
+<td align="left"><input type="text" name="txtNuContato" /></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Tel. Contato 2:</label> </td>
+<td align="left"><input type="text" name="txtNuContato2" /></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Número SUS:</label> </td>
+<td align="left"><input type="text" name="txtNuSus" /></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Logradouro:</label> </td>
+<td align="left"><input type="text" size="70" name="txtNmRua" required/></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Número:</label> </td>
+<td align="left"><input type="text" size="6" name="txtNumero" required/></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Bairro:</label> </td>
+<td align="left"><input type="text" name="txtBairro" /></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">Cidade:</label> </td>
+<td align="left"><input type="text" name="txtCidade" /></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">UF</label> </td>
+<td><select id="cbxUf" name="cbxUf" required>
+<option value="">Nenhuma</option>
+<option value="SC">SC</option>
+  <option value="RS">RS</option>
+  <option value="SP">SP</option>
+  <option value="RJ">RJ</option>
+  <option value="MA">MA</option>
+</select></td>
+</tr>
+
+<tr>
+<td align="right"><label class="fonte">CEP:</label> </td>
+<td align="left"><input type="text" name="txtCep" required/></td>
+</tr>
+
+<tr>
+<td>
+
+</td>
+</tr>
+</table>
+
+
+
+<table id="tblBotao">
+<tr>
+<td width="965px" align="right">
+<input  type="submit" value="Vamos Lá" />
+
+</td>
+</tr>
+</table>
+
+</td>
+</tr>
+</table>
+
+
+</form>
+
+</body>
+</html>
---
+++ src/br/com/healthtime/servelet/CadastroUsuario.java
@@ -0,0 +1,125 @@
+package br.com.healthtime.servelet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.healthtime.dao.UsuarioDAO;
+import br.com.healthtime.entity.Endereco;
+import br.com.healthtime.entity.Usuario;
+
+
+
+/**
+ * Servlet implementation class CadastroUsuario
+ */
+@WebServlet("/CadastroUsuario")
+public class CadastroUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CadastroUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request, response);
+	}
+	
+	private void validaDadosRecebidos(HttpServletRequest req) throws ServletException {
+
+		// Cadastrar Usuario
+
+		String nmUsuario = req.getParameter("txtNome");
+		String nuCpf = req.getParameter("txtNuCpf");
+		String dtNascimento = req.getParameter("txtDtNascimento");
+		String email = req.getParameter("txtemail");
+		String flGestante = req.getParameter("flGestante");
+		String flIdoso = req.getParameter("flIdoso");
+		String flMenorIdade = req.getParameter("flMenorIdade");
+		String flPne = req.getParameter("flPne");
+		String nmMae = req.getParameter("txtNmMae");
+		String nuContato = req.getParameter("txtNuContato");
+		String nuContato2 = req.getParameter("txtNuContato2");
+		String nuSus = req.getParameter("txtNuSus");
+		String nmRua = req.getParameter("txtNmRua");
+		String numero = req.getParameter("txtNumero");
+		String nmBairro = req.getParameter("txtBairro");
+		String nmCidade = req.getParameter("txtCidade");
+		String UF = req.getParameter("cbxUf");
+		String nuCep = req.getParameter("txtCep");
+		
+		
+
+			try {
+
+				Usuario usuario = new Usuario();
+				usuario.setCodigo(1);
+				usuario.setNome(nmUsuario);
+				usuario.setCpf(nuCpf);
+				usuario.setDtnascimento(Integer.parseInt(dtNascimento));
+				usuario.setNuContatoPrincipal(nuContato);
+				usuario.setNomeMae(nmMae);
+				usuario.setEmail(email);
+				usuario.setFlGestante(Boolean.parseBoolean(flGestante));
+				usuario.setFlIdoso(Boolean.parseBoolean(flIdoso));
+				usuario.setFlMenorIdade(Boolean.parseBoolean(flMenorIdade));
+				usuario.setFlPne(Boolean.parseBoolean(flPne));
+				usuario.setSus(nuSus);
+				
+				Endereco endereco = new Endereco();
+				endereco.setBairro(nmBairro);
+				endereco.setCep(nuCep);
+				endereco.setCidade(nmCidade);
+				endereco.setNumero(Integer.parseInt(numero));
+				endereco.setRua(nmRua);
+				endereco.setUf(UF);
+				usuario.setEndereco(endereco);
+				
+				
+				UsuarioDAO dao = new UsuarioDAO();
+				dao.salvaUsuario(usuario);;
+				
+
+			} catch (Exception e) {
+				System.out.println("Erro: " + e);
+			}
+
+		}
+	
+private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		try {
+			validaDadosRecebidos(req);
+			RequestDispatcher rd = req.getRequestDispatcher("Login.jsp");
+			rd.forward(req, resp);
+
+		} catch (Exception e) {
+			RequestDispatcher rd = req.getRequestDispatcher("ErroLogin.jsp");
+			rd.forward(req, resp);
+		}
+	
+	}
+}
